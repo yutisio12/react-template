480be8f2b92c2d2ca7b259749250498368787dc8
yutisio12 <yutisiogamings@gmail.com>
2026-05-30 16:41:30 +0700

    FE:
    - rectifying global css
    - rectifying layout js

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="antialiased min-h-screen flex flex-col">
         <AuthProvider>
           {children}
